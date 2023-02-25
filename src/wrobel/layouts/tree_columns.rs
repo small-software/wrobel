3d366947bb4011369152ce51862b61a9424a0f71
Grzegorz Bylica <grzegorz.bylica@gmail.com>
2023-02-25 12:07:39 +0100

    I have added stylist

--- src/wrobel/layouts/tree_columns.rs
+++ src/wrobel/layouts/tree_columns.rs
@@ -1,6 +1,7 @@
 use stylist::css;
 use stylist::yew::Global;
 use yew::prelude::*;
+use stylist::style;
 
 
 #[derive(Properties, PartialEq)]
@@ -14,13 +15,28 @@ pub struct Props {
 pub fn TreeColumns(props: &Props) -> Html {
     html! {
 
-        <div class={css!(r#"
+        <Global css=r#"
+            .page-wrap {
                 display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
                 display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
                 display: -ms-flexbox;      /* TWEENER - IE 10 */
                 display: -webkit-flex;     /* NEW - Chrome */
                 display: flex;             /* NEW, Spec - Opera 12.1, Firefox 20+ */
-        "#)}>
+            }
+            .main-content {
+                width: 60%;
+            }
+            .main-nav,
+            .main-sidebar {
+                -webkit-box-flex: 1;      /* OLD - iOS 6-, Safari 3.1-6 */
+                -moz-box-flex: 1;         /* OLD - Firefox 19- */
+                width: 20%;               /* For old syntax, otherwise collapses. */
+                -webkit-flex: 1;          /* Chrome */
+                -ms-flex: 1;              /* IE 10 */
+                flex: 1;                  /* NEW, Spec - Opera 12.1, Firefox 20+ */
+            }"# />
+        //class={css!(r#""#)}
+        <div class="page-wrap">
             <section class="main-content" role="main">
                 {props.main_content.to_owned()}
             </section>
