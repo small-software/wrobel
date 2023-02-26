a7076df49c8be21c3cd06da9860b3397573c5e2c
Grzegorz Bylica <grzegorz.bylica@gmail.com>
2023-02-26 22:41:16 +0100

    I have added btn

--- src/wrobel/styles/btn.rs
+++ src/wrobel/styles/btn.rs
@@ -10,13 +10,14 @@ pub fn Btn() -> Html {
                 .btn {
                     padding: .3em .8em;
                     border: 1px solid #446d88;
-                    background: #58a linear-gradient(#77a0bb, #58a); /* border-radius: .2em; */
+                    background: #58a linear-gradient(#77a0bb, #58a);
+                    /* border-radius: .2em;  */
                     box-shadow: 0 .05em .25em gray;
                     color: white;
                     text-shadow: 0 -.05em .05em #335166;
-                    font-size 125%;
+                    font-size: 125%;
                     line-height: 1.5;
-                }
+            }
             "#);
 
     html! {
